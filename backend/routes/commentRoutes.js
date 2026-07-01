b1cd9d8ec001e2e7addbe4421040c00e37a4933b
ycs3858 <yangcs002@naver.com>
2026-07-01 16:54:55 +0900

    토큰 경로 추가

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const commentController = require('../controllers/commentController');
 const { verify } = require('jsonwebtoken');
 
+const { verifyToken } = require('../middlewares/authMiddleware');
+
 router.get('/:postId', commentController.getComments);
 
 router.post('/', commentController.createComment);
